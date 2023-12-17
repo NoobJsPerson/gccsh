afa54b2fecfd66b8a9bff94d01fbf509a52a4e65
NoobJsPerson <itsmeac101@gmail.com>
2023-12-17 17:55:46 +0100

    add exit, cmd and version and enhanced readme

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # gccsh
 use C in the shell (WIP)
+### features
+- execute C code in a shell-like environment
+- familiar builtins like `chdir` and `exit`
+- `cmd` builtin allows you to run a command in the default shell and returns the output.
+- `version` variable is provided as a c string
+### known issues
+- can only use `chdir` and `exit`  properly once and it will always run before the rest of the code
+
--- main.c
+++ main.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
+#include <unistd.h>
 #include <stdlib.h>
 #include <string.h>
+#include <regex.h>
 
 #define GCCSH_RL_BUFSIZE 1024
 
-char *gccsh_readline(void) {
+char *gccsh_readline(int *line_len) {
 	int bufsize = GCCSH_RL_BUFSIZE;
 	int position = 0;
 	char *buffer = malloc(sizeof(char) * bufsize);
@@ -18,7 +20,8 @@ char *gccsh_readline(void) {
 	while(1) {
 		c = getchar();
 		if( c == '\n') {
-			buffer[position] = '\n';
+			buffer[position] = '\0';
+			*line_len = position + 1;
 			return buffer;
 		} else {
 			buffer[position] = c;
@@ -28,20 +31,55 @@ char *gccsh_readline(void) {
 			bufsize += GCCSH_RL_BUFSIZE;
 			buffer = realloc(buffer, bufsize);
 			if(!buffer) {
-				fprintf(stderr,"lsh: allocation error");
+				fprintf(stderr,"gccsh: allocation error");
 				exit(EXIT_FAILURE);
 			}
 		}
 	}
 }
-
+void gccsh_match_builtins(char* line, int line_len) {
+	regex_t regex;
+	regmatch_t matches[2];
+	int reti;
+	char *msgbuf = calloc(sizeof(char), line_len);
+	reti = regcomp(&regex, "chdir\((.+));", REG_EXTENDED);
+	if (reti) {
+		fprintf(stderr, "Could not compile regex\n");
+		exit(EXIT_FAILURE);
+	}
+	reti = regexec(&regex, line, 2, matches, 0);
+	if(reti == 0) {
+		// Match
+		memcpy(msgbuf, line + matches[1].rm_so + 2, matches[1].rm_eo - matches[1].rm_so - 4);
+		chdir(msgbuf);
+	} else if(reti != REG_NOMATCH) {
+		regerror(reti, &regex, msgbuf, sizeof(msgbuf));
+		exit(EXIT_FAILURE);
+	}
+	reti = regcomp(&regex, "exit\(.+);", REG_EXTENDED);
+	if (reti) {
+		fprintf(stderr, "Could not compile regex\n");
+		exit(EXIT_FAILURE);
+	}
+	reti = regexec(&regex, line, 0, NULL, 0);
+	if(reti == 0) {
+		exit(EXIT_SUCCESS);
+	} else if (reti != REG_NOMATCH) {
+		regerror(reti, &regex, msgbuf, sizeof(msgbuf));
+		exit(EXIT_FAILURE);
+	}
+	regfree(&regex);
+	free(msgbuf);
+}
 void gccsh_loop() {
 	char *line;
+	int line_len;
 	int status = 1;
 	FILE *fptr;
 	do {
 		printf("> ");
-		line = gccsh_readline();
+		line = gccsh_readline(&line_len);
+		gccsh_match_builtins(line, line_len);
 		fptr = fopen(".gccshtemp.c","w");
 		if(fptr == NULL) {
 			perror("fopen");
@@ -49,7 +87,22 @@ void gccsh_loop() {
 		}
 		fprintf(fptr,"#include <stdio.h>\n"
 				"#include <unistd.h>\n"
-				"int main() { %s }", line);
+				"#include <stdlib.h>\n"
+				"char* cmd(char* command) {\n"
+				"FILE *fp; char buffer[1024]; char* result = NULL; size_t result_size = 0;"
+				"fp = popen(command, \"r\");"
+				"if (fp == NULL) {"
+				"perror(\"Error opening pipe\");"
+				"return NULL;"
+				"}\n"
+				"while (fgets(buffer, sizeof(buffer), fp) != NULL) {"
+				"result_size += strlen(buffer) * sizeof(char);"
+				"result = realloc(result, result_size);"
+				"strcat(result, buffer);"
+				"}\n"
+				"pclose(fp); return result;"
+				"}\n"
+				"int main() { char version[] = \"0.1.0\"; %s }", line);
 		fclose(fptr);
 		int compSuccess = system("gcc .gccshtemp.c -o .gccshtemp.bin");
 		if(compSuccess == 0) {
